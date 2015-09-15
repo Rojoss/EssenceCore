4c8fe3f4ec6bc7a583d523982b14dcf47a8a5f43
Jos <josroossien@hotmail.com>
2015-09-15 12:04:30 +0100

    ES-18 #close

--- essence/src/main/java/info/mcessence/essence/config/MessagesCfg.java
+++ essence/src/main/java/info/mcessence/essence/config/MessagesCfg.java
@@ -54,6 +54,7 @@ public class MessagesCfg extends EasyConfig {
             }
             Map<String, String> messages = MESSAGES.get(category);
             List<String> keys = new ArrayList<>(messages.keySet());
+            //Testing JIRA commits.
             for (String msg : keys) {
                 if (Message.fromString(msg) == null) {
                     messages.remove(msg);

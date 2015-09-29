4a4cf5cd68f1e74c796a5d4afa2e08d8670a78a2
Jos <josroossien@hotmail.com>
2015-09-29 23:53:14 +0100

    %Update copyright to use essencemc.org (Please everyone update your automated copyright for Essence with this change)

--- essencecore/src/main/java/org/essencemc/essencecore/EssenceCore.java
+++ essencecore/src/main/java/org/essencemc/essencecore/EssenceCore.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Essence <http://www.mc-essence.info>
+ * Copyright (c) 2015 Essence <http://essencemc.org>
  * Copyright (c) 2015 contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy

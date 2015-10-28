cf3f5acc668b3665ecf28987d9ebbcb3c1882e05
The-King <hunar911@gmail.com>
2015-10-28 15:41:31 -0700

    %Finished Builder for titles

--- api/src/main/java/org/essencemc/essencecore/nms/packet/playout/title/Builder.java
+++ api/src/main/java/org/essencemc/essencecore/nms/packet/playout/title/Builder.java
@@ -25,16 +25,109 @@
 
 package org.essencemc.essencecore.nms.packet.playout.title;
 
+import org.bukkit.entity.Player;
 import org.essencemc.essencecore.plugin.NMSFetcher;
 
+import java.util.Collection;
+
 /**
- *
+ * Builder for titles and subtitles
  */
 public class Builder {
 
-    private final NMSFetcher inmsFetcher;
+    private final NMSFetcher nmsFetcher;
 
-    public Builder(NMSFetcher inmsFetcher) {
-        this.inmsFetcher = inmsFetcher;
+    private int titleFadeIn = 20;
+    private int titleStay = 30;
+    private int titleFadeOut = 20;
+
+    private int subtitleFadeIn = 20;
+    private int subtitleStay = 30;
+    private int subtitleFadeOut = 20;
+
+    private String titleMessage = "";
+    private String subtitleMessage = "";
+
+    private Builder() {
+        this.nmsFetcher = null;
+    }
+
+    public Builder(NMSFetcher nmsFetcher) {
+        this.nmsFetcher = nmsFetcher;
+    }
+
+    public Builder setTimings(int fadeIn, int stay, int fadeOut) {
+        titleFadeIn = fadeIn;
+        titleStay = stay;
+        titleFadeOut = fadeOut;
+
+        subtitleFadeIn = fadeIn;
+        subtitleStay = stay;
+        subtitleFadeOut = fadeOut;
+
+        return this;
+    }
+
+    public Builder setTitleTimings(int fadeIn, int stay, int fadeOut) {
+        titleFadeIn = fadeIn;
+        titleStay = stay;
+        titleFadeOut = fadeOut;
+
+        return this;
+    }
+
+    public Builder setSubtitleTimings(int fadeIn, int stay, int fadeOut) {
+
+        subtitleFadeIn = fadeIn;
+        subtitleStay = stay;
+        subtitleFadeOut = fadeOut;
+
+        return this;
+    }
+
+    public Builder setTitleMessage(String titleMessage) {
+        this.titleMessage = titleMessage;
+        return this;
+    }
+
+    public Builder setSubtitleMessage(String subtitleMessage) {
+        this.subtitleMessage = subtitleMessage;
+        return this;
+    }
+
+    public Builder send(Player player) {
+        if (!titleMessage.equals("")) {
+            nmsFetcher.getTitle().sendTitle(titleMessage, titleFadeIn, titleStay, titleFadeOut, player);
+        }
+
+        if (!subtitleMessage.equals("")) {
+            nmsFetcher.getTitle().sendSubtitle(subtitleMessage, subtitleFadeIn, subtitleStay, subtitleFadeOut, player);
+        }
+
+        return this;
+    }
+
+    public Builder send(Player[] players) {
+        if (!titleMessage.equals("")) {
+            nmsFetcher.getTitle().sendTitle(titleMessage, titleFadeIn, titleStay, titleFadeOut, players);
+        }
+
+        if (!subtitleMessage.equals("")) {
+            nmsFetcher.getTitle().sendSubtitle(subtitleMessage, subtitleFadeIn, subtitleStay, subtitleFadeOut, players);
+        }
+
+        return this;
+    }
+
+    public Builder send(Collection<? extends Player> players) {
+        if (!titleMessage.equals("")) {
+            nmsFetcher.getTitle().sendTitle(titleMessage, titleFadeIn, titleStay, titleFadeOut, players);
+        }
+
+        if (!subtitleMessage.equals("")) {
+            nmsFetcher.getTitle().sendSubtitle(subtitleMessage, subtitleFadeIn, subtitleStay, subtitleFadeOut, players);
+        }
+
+        return this;
     }
 }
